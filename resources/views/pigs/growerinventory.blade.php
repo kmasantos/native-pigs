a92365616fbb05189d3c7d3a93d7741d011b829b
laubsofficial <laubreyual@gmail.com>
2018-04-05 10:43:56 +0800

    Initial UI for grower inventory page

--- resources/views/pigs/growerinventory.blade.php
+++ resources/views/pigs/growerinventory.blade.php
@@ -9,7 +9,47 @@
 		<h4>Grower Inventory Report</h4>
 		<div class="divider"></div>
 		<div class="row center" style="padding-top: 10px;">
-			
+			<canvas id="growerinventorycanvas"></canvas>
+			<h5>Age, months</h5>
 		</div>
 	</div>
+@endsection
+
+@section('scripts')
+	<script>
+		var ctx1 = document.getElementById("growerinventorycanvas").getContext('2d');
+		var growerinventorychart = new Chart(ctx1, {
+			"type":"bar",
+			"data": {
+				"labels":["0","1","2","3","4","5","6"],
+				"datasets":[
+					{
+						"label":"Sows",
+						"data":[2,3,4,5,1,3,6],
+						"fill":false,
+						"backgroundColor": "rgba(255, 99, 132, 0.2)",
+						"borderColor": "rgb(255, 99, 132)",
+						"borderWidth":1
+					},
+					{
+						"label": "Boars",
+						"data":[2,1,1,4,3,0,1],
+						"fill":false,
+						"backgroundColor": "rgba(75, 192, 192, 0.2)",
+						"borderColor": "rgb(75, 192, 192)",
+						"borderWidth":1
+					}
+				],
+			},
+			"options":{
+				"scales":{
+					"yAxes":[{
+						"ticks":{
+							"beginAtZero":true
+						}
+					}]
+				}
+			}
+		});
+	</script>
 @endsection
