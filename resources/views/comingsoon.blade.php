dd1256f624373d683ae9a7d824d761520d63519f
laubsofficial <laubreyual@gmail.com>
2019-05-09 13:37:18 +0800

    Added a coming soon page

---
+++ resources/views/comingsoon.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>PAB-IS: Native Pigs</title>
+        <link rel="shortcut icon" href="images/logo-swine-square.png" />
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+        <!-- Styles -->
+        {{-- <link rel="stylesheet" href="{{asset('thirdparty/materialize/css/materialize.min.css')}}"> --}}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-alpha.1/css/materialize.min.css">
+        {{-- <link rel="stylesheet" href="{{asset('css/global.css')}}"> --}}
+        {{-- <link type="text/css" rel="stylesheet" href="{{asset('css/pig.css')}}"  media="screen,projection"/> --}}
+    </head>
+    <body>
+        <div class="container">
+            <div class="content center">
+                <div class="col s12 m12 l12">
+                    <img src="{{asset('images/working.gif')}}" width="50%">
+                    <h3>COMING SOON!</h3>
+                    <p>Click <a href="{{url('/')}}">here</a> to go back</p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
