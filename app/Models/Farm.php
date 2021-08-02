626d45da999e0dff4be42b8549cd9c3c1d806235
kmasantos <kmabriolsantos@up.edu.ph>
2021-08-02 10:34:51 +0800

    support soft deletion

--- app/Models/Farm.php
+++ app/Models/Farm.php
@@ -6,10 +6,12 @@ use App\Models\Breed;
 use DB;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Farm extends Model
 {
-
+  use SoftDeletes;
+  
   protected $table = 'farms';
   public $timestamps = false;
   protected $fillable = [
