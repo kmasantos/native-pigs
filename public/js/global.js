469fabd59940d74d8fbca7f3e006df9fbf46ab12
kmasantos <kmabriolsantos@up.edu.ph>
2021-12-21 01:22:55 +0800

    disable datepicker

--- public/js/global.js
+++ public/js/global.js
@@ -3,15 +3,15 @@ $(document).ready(function(){
   // $('.modal-trigger').leanModal();
   $('select').material_select();
 
-  $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: 'Today',
-    clear: 'Clear',
-    format: 'yyyy-mm-dd',
-    close: 'Ok',
-    closeOnSelect: false // Close upon selecting a date,
-  });
+//   $('.datepicker').pickadate({
+//     selectMonths: true, // Creates a dropdown to control month
+//     selectYears: 15, // Creates a dropdown of 15 years to control year,
+//     today: 'Today',
+//     clear: 'Clear',
+//     format: 'yyyy-mm-dd',
+//     close: 'Ok',
+//     closeOnSelect: false // Close upon selecting a date,
+//   });
 
   $('.modal').modal({
     dismissible: true, // Modal can be dismissed by clicking outside of the modal
