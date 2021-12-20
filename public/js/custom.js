ddd7adab15a4e4e50b785eb7f23da178d294da9d
kmasantos <kmabriolsantos@up.edu.ph>
2021-12-21 01:21:29 +0800

    disable picker

--- public/js/custom.js
+++ public/js/custom.js
@@ -38,11 +38,11 @@ $('.showSideNav').sideNav({
   closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
 });
 
-$('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: 'Today',
-    clear: 'Clear',
-    close: 'Ok',
-    closeOnSelect: false // Close upon selecting a date,
-  });
+// $('.datepicker').pickadate({
+//     selectMonths: true, // Creates a dropdown to control month
+//     selectYears: 15, // Creates a dropdown of 15 years to control year,
+//     today: 'Today',
+//     clear: 'Clear',
+//     close: 'Ok',
+//     closeOnSelect: false // Close upon selecting a date,
+//   });
